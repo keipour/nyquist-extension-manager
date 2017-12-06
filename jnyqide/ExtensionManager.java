6774154da84b1ca8e0ac6dfe5ee8c2944691fe46
Azarakhsh <keipour@gmail.com>
2017-12-06 11:10:30 -0500

    Added UI for Extension Manager

--- jnyqide/ExtensionManager.java
+++ jnyqide/ExtensionManager.java
@@ -1,5 +1,6 @@
 package jnyqide;
 
+
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
@@ -7,14 +8,16 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 import javax.swing.AbstractListModel;
-
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ListSelectionModel;
 
 public class ExtensionManager extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private final JList list = new JList();
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -38,25 +41,73 @@ public class ExtensionManager extends JDialog {
 		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.add(list);
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"aaaaaaaaa", "bbbbbbbb"};
-			public int getSize() {
-				return values.length;
+/*		{
+			table.getColumnModel().getColumn(0).setPreferredWidth(182);
+			table.getColumnModel().getColumn(1).setPreferredWidth(65);
+			table.getColumnModel().getColumn(3).setPreferredWidth(302);
+			table.getColumnModel().getColumn(4).setPreferredWidth(212);
+			getContentPane().add(table, BorderLayout.NORTH);
+		}
+		getContentPane().add(new JScrollPane(table));
+*/
+		{
+			table = new JTable();
+			table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+			table.setCellSelectionEnabled(false);
+			table.setColumnSelectionAllowed(false);
+			table.setRowSelectionAllowed(true);
+			table.setModel(new DefaultTableModel(
+					new Object[][] {
+							{null, null, null, null, null},
+							{null, null, null, null, null},
+							{null, null, null, null, null},
+							{null, null, null, null, null},
+							{null, null, null, null, null},
+						},
+						new String[] {
+							"Package", "Version", "Date", "Description", "URL"
+						}
+			)
+			{
+				Class[] columnTypes = new Class[] {
+					String.class, String.class, String.class, String.class, String.class
+				};
+				public Class getColumnClass(int columnIndex) {
+					return columnTypes[columnIndex];
+				}
+				boolean[] columnEditables = new boolean[] {
+					false, false, false, false, false
+				};
+				public boolean isCellEditable(int row, int column) {
+					return columnEditables[column];
+				}
+			});
+			{
+				table.getColumnModel().getColumn(0).setPreferredWidth(182);
+				table.getColumnModel().getColumn(1).setPreferredWidth(65);
+				table.getColumnModel().getColumn(3).setPreferredWidth(302);
+				table.getColumnModel().getColumn(4).setPreferredWidth(212);
+				getContentPane().add(table, BorderLayout.NORTH);
 			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
+			getContentPane().add(new JScrollPane(table));
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton addButton = new JButton("Add Extensions");
+				addButton.setActionCommand("Add");
+				buttonPane.add(addButton);
+				getRootPane().setDefaultButton(addButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
@@ -65,5 +116,4 @@ public class ExtensionManager extends JDialog {
 			}
 		}
 	}
-
 }
